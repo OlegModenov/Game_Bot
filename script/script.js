36701a5cac27461315f803c7f45ac8ff1f46ca01
Oleg <olegmodenov@gmail.com>
2020-01-30 20:43:10 +0500

    Сделан начальный функционал

--- script/script.js
+++ script/script.js
@@ -1,2 +1,24 @@
 'use strict';
 
+let isNumber = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+let game = function () {
+  let answer = prompt('Угадай число от 1 до 100');
+  let number = 45;
+  if (answer == null) {
+    alert('Спасибо за игру!');
+  } else if (!isNumber(answer)) {
+    alert('Введи число!');
+    game();
+  } else if (answer > 100) {
+    alert('Загаданное число меньше');
+    game();
+  } else if (answer < 0) {
+    alert('Загаданное число больше');
+    game();
+  }
+};
+
+game();
